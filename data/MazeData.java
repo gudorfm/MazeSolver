fbdde6ea81368655a36b287e7847b129d5bd1ed4
Mark Gudorf <gudorfm@gmail.com>
2022-11-30 14:58:34 -0500

    Changed data type of variables in MazeData objext to allow parsing.  Added parsing to get maze data method.

--- data/MazeData.java
+++ data/MazeData.java
@@ -1,16 +1,16 @@
 package data;
 
 public class MazeData {
-    public int bumps;
-    public int coins;
-    public int id;
-    public int secondsLeft;
-    public int size;
+    public long bumps;
+    public long coins;
+    public long id;
+    public long secondsLeft;
+    public long size;
     public String status;
     public String username;
 
     @Override
     public String toString(){
-        return ("MazeData/nBumps: " + bumps + " Coins: " + coins + " Seconds Left: " + secondsLeft);
+        return ("MazeData\nBumps: " + bumps + " Coins: " + coins + " Seconds Left: " + secondsLeft);
     }
 }
